4df6356829fc9a25a9ad7297048941ff159c1243
Bethwel Kiplagat <sjemnyango9@gmail.com>
2025-07-02 23:06:34 +0300

    Added routes to Auth

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import { ThemeProvider } from './context/ThemeContext';
+
+const App = () => {
+  return (
+    <ThemeProvider>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </Router>
+    </ThemeProvider>
+  );
+};
+
+export default App;
